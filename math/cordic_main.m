72809463b9cfb9c0dff74a269c8c7ac0582ec850
Alexander Kapitanov <sallador@bk.ru>
2018-09-18 15:32:10 +0300

    Update .m files: change cordic algorithm

--- math/cordic_main.m
+++ math/cordic_main.m
@@ -13,84 +13,91 @@ clear all;
 close all;
 
 % Set: Phase vector size (phase - is a bit-vector)
-NPHI = 12;
+NPHI = 11;
 % Set: Gain of signal (output width in HDL)
-NOUT = 15;
+NOUT = 12;
 %GAIN = 2^(NOUT-1)/1.6467;
 GAIN = 2^(NOUT-1)/1.6467;
 
 
 
 % ---- Calculate angles and create Look up table for ATAN -------------------- %
-Angle(:,1) = round(atan(2.^(-(0:(NOUT-1)))) .* (2^(NPHI)/(2*pi)));
-Angle_Int = int32(Angle);
+Angle(:,1) = round( atan(2.^(-(0:NOUT))) .* (2^(NOUT)/pi ) );
+Angle(length(Angle),1) = 0;
 
-Magn = prod(sqrt(1+2.^(-2*(0:(NOUT-1)))));
+Angle_Int = int32(Angle);
+Angle_Hex = dec2hex(Angle_Int);
+
+Magn = prod(sqrt(1+2.^(-2*(0:(NOUT)))));
 % ---- Calculate quadrant from input phase ----------------------------------- %
-function quad_out = find_quad(Phase, Gain, PHI)
-  x_ret = 0; y_ret = 0; z_ret = 0;
+function quad_out = find_quad(Phase, PHI, NDAT)
+  z_ret = 0; r_ret = 0;
   
-  if ((Phase > 0) && (Phase < 2^(PHI-2))) % MSBs: "00" in HDL
+  if ((Phase > 0) && (Phase < 2^(PHI-1)/2)) % MSBs: "00" in HDL 
     z_ret = Phase;
-    x_ret = Gain;
-    y_ret = 0;
-  elseif ((Phase > 2^(PHI-2)-1) && (Phase < 2^(PHI-1))) % MSBs: "01" in HDL
-    z_ret = Phase - 2^(PHI-2);
-    x_ret = 0;
-    y_ret = Gain;
-  elseif ((Phase > -2^(PHI-2)+1) && (Phase < 1)) % MSBs: "11" in HDL 
-    z_ret = Phase;
-    x_ret = Gain;
-    y_ret = 0;
+  elseif ((Phase > 2^(PHI-1)/2-1) && (Phase < 2^(PHI-1))) % MSBs: "01" in HDL
+    z_ret = Phase - 2^(PHI-1);
+    r_ret = 1;
+  elseif ((Phase > -2^(PHI-1)-1) && (Phase < -2^(PHI-1)/2)) % MSBs: "11" in HDL 
+    z_ret = Phase + 2^(PHI-1);
+    r_ret = 1;
   else % MSBs: "10" in HDL 
-    z_ret = Phase + 2^(PHI-2);
-    x_ret = 0;
-    y_ret = -Gain;    
+    z_ret = Phase;
   endif
-  quad_out(1,1) = int32(x_ret);
-  quad_out(1,2) = int32(y_ret);
-  quad_out(1,3) = int32(z_ret);
+  z_ret = round(z_ret * 2^(NDAT-PHI+1));
+  quad_out(1,1) = int32(z_ret);
+  quad_out(1,2) = int32(r_ret);
 end
 
 % ---- Calculate sin / cos values -------------------------------------------- %
-function harmonic_out = find_wave(Data, GainX, GainY, LutAtan, Len)
-  sig_ret(1) = (Data);
-  cos_ret(1) = (GainX);
-  sin_ret(1) = (GainY);
-  for ii = 1:(Len-1)
-    if (sig_ret(ii) < 0)
-      sig_ret(ii+1) = sig_ret(ii) + LutAtan(ii);
-      cos_ret(ii+1) = cos_ret(ii) + floor(sin_ret(ii) * 2^(1-ii));
-      sin_ret(ii+1) = sin_ret(ii) - floor(cos_ret(ii) * 2^(1-ii));      
+function harmonic_out = find_wave(Data, Rot, LutAtan, Len, Phi)
+%  sig_ret = double(Data);
+  sig_ret = Data;
+  cos_ret = 2^(Len-1);
+  sin_ret = 0;
+  
+  for ii = 0:Len
+    if (sig_ret < 0)
+      sig_ret = sig_ret + LutAtan(ii+1);
+      cos_new = cos_ret + round(sin_ret * 2^(-ii));
+      sin_new = sin_ret - round(cos_ret * 2^(-ii));
+%      sig_ret = sig_ret + 2^(Phi-3) * atan(2^-ii) / pi;      
     else
-      sig_ret(ii+1) = sig_ret(ii) - LutAtan(ii);
-      cos_ret(ii+1) = cos_ret(ii) - floor(sin_ret(ii) * 2^(1-ii));
-      sin_ret(ii+1) = sin_ret(ii) + floor(cos_ret(ii) * 2^(1-ii));      
+      sig_ret = sig_ret - LutAtan(ii+1);
+      cos_new = cos_ret - round(sin_ret * 2^(-ii));
+      sin_new = sin_ret + round(cos_ret * 2^(-ii)); 
+%      sig_ret = sig_ret - 2^(Phi-3) * atan(2^-ii) / pi;     
     endif
+    cos_ret = cos_new;
+    sin_ret = sin_new;
   endfor
-  harmonic_out(1,1) = (cos_ret(Len-1));
-  harmonic_out(1,2) = (sin_ret(Len-1));
+  
+  if (Rot == 1)
+		cos_ret = -cos_ret;
+		sin_ret = -sin_ret;  
+  end
+  
+  
+  harmonic_out(1,1) = round(cos_ret / 1.6467);
+  harmonic_out(1,2) = round(sin_ret / 1.6467);
 end
 
 % ---- Create phase vector and calculate sin / cos --------------------------- % 
-%phi_1 = 0:8:2^(NPHI-1)-1;
-%phi_2 = -2^(NPHI-1):8:-1;
 
-STEP = 1;
+phi_ii = -2^(NPHI-1) : 1 : 2^(NPHI-1)-1;
+phi_1 = -2^(NPHI-1) : 1 : -1;
+phi_2 = 0 : 1 : 2^(NPHI-1)-1;
 
-phi_ii = -2^(NPHI-1):STEP:2^(NPHI-1)-1;
-%phi_ii = [phi_1 phi_2];
-
-%phi_ii = int32(phi_ii);
+phi_ii = [phi_2 phi_1];
 
 for ii = 1:length(phi_ii)
-  phi_out(ii,:) = find_quad(phi_ii(1,ii), GAIN, NPHI);
-  sig_out(ii,:) = find_wave(phi_out(ii,3), phi_out(ii,1), phi_out(ii,2), Angle_Int, NOUT);
+  phi_out(ii,:) = find_quad(phi_ii(1,ii), NPHI, NOUT);
+  sig_out(ii,:) = find_wave(phi_out(ii,1), phi_out(ii,2), Angle, NOUT, NPHI);
 endfor
 
 %PhX = phi_out(:,1);
 %PhY = phi_out(:,2);
-%PhZ = phi_out(:,3);
+PhZ = phi_out(:,1);
 ReSig = sig_out(:,1);
 ImSig = sig_out(:,2);
 
@@ -101,24 +108,24 @@ max(abs(ReSig))
 % ---- Calculate spectrum for CORDIC sine / cosine --------------------------- % 
 %Spec_Re = fft(ReSig, 2^(NPHI)); 
 %Spec_Im = fft(ImSig, 2^(NPHI)); 
-Spec_Re = fft(ReSig + 1 * randn(size(ReSig)), length(phi_ii)); 
-Spec_Im = fft(ImSig + 1 * randn(size(ImSig)), length(phi_ii));
+Spec_Re = fft(ReSig + 0.1 * randn(size(ReSig)), length(phi_ii)); 
+Spec_Im = fft(ImSig + 0.1 * randn(size(ImSig)), length(phi_ii));
 Spec_Re = Spec_Re .* conj(Spec_Re);
 Spec_Im = Spec_Im .* conj(Spec_Im);
 
-Spec_Re = fftshift(Spec_Re(2:end)); 
-Spec_Im = fftshift(Spec_Im(2:end));
+Spec_Re = fftshift(Spec_Re); 
+Spec_Im = fftshift(Spec_Im);
 
 Sabs_Re = Spec_Re / max(Spec_Re);
 Sabs_Im = Spec_Im / max(Spec_Im);
 
-Slog_Re = 10*log10(Sabs_Re+1e-10);
-Slog_Im = 10*log10(Sabs_Im+1e-10);
+Slog_Re = 10*log10(Sabs_Re);
+Slog_Im = 10*log10(Sabs_Im);
 
 
 % ---- Calculate ideal values of sine / cosine ------------------------------- % 
-Id_sin(:,1) = round(-2^(NOUT-1) * sin((0:(2^NPHI/STEP-1)).*2*pi/(2^NPHI/STEP)));
-Id_cos(:,1) = round(-2^(NOUT-1) * cos((0:(2^NPHI/STEP-1)).*2*pi/(2^NPHI/STEP)));
+Id_sin(:,1) = round(2^(NOUT-1) * sin( ( 0: 2^NPHI-1 ) .* 2*pi/(2^NPHI)));
+Id_cos(:,1) = round(2^(NOUT-1) * cos( ( 0: 2^NPHI-1 ) .* 2*pi/(2^NPHI)));
 
 Id_sin(:,1) = Id_sin + 1 * randn(size(Id_sin));
 Id_cos(:,1) = Id_cos + 1 * randn(size(Id_cos));
@@ -132,22 +139,22 @@ Spec_Im = fft(Id_sin, length(phi_ii));
 Spec_Re = Spec_Re .* conj(Spec_Re);
 Spec_Im = Spec_Im .* conj(Spec_Im);
 
-Spec_Re = fftshift(Spec_Re(2:end)); 
-Spec_Im = fftshift(Spec_Im(2:end));
+Spec_Re = fftshift(Spec_Re); 
+Spec_Im = fftshift(Spec_Im);
 
 Sabs_Re = Spec_Re / max(Spec_Re);
 Sabs_Im = Spec_Im / max(Spec_Im);
 
-Sidl_Re = 10*log10(Sabs_Re+1e-10);
-Sidl_Im = 10*log10(Sabs_Im+1e-10);
+Sidl_Re = 10*log10(Sabs_Re);
+Sidl_Im = 10*log10(Sabs_Im);
 
 % ---- Plot figures ---------------------------------------------------------- % 
 figure(1) % Plot loaded data in Freq Domain
-  subplot(4,1,1)
+  subplot(1,1,1)
   plot(ReSig, '-', 'LineWidth', 1, 'Color',[1 0 0])
-  grid on; hold on; axis tight; 
+  grid on; hold on; axis ([450, 580, 2010, 2050]); 
   plot(ImSig, '-', 'LineWidth', 1, 'Color',[0 0 1])
-  grid on; hold on; axis tight; 
+  grid on; hold on; axis ([450, 580, 2010, 2050]); 
   title(['CORDIC SINE / COSINE:'])
 
 %  subplot(3,1,2)
@@ -156,26 +163,26 @@ figure(1) % Plot loaded data in Freq Domain
 %  plot(Sabs_Im, '-', 'LineWidth', 1, 'Color',[0 0 1])
 %  grid on; hold on; axis tight;   
   
-  subplot(4,1,2)
-  plot(Slog_Re, '-', 'LineWidth', 1, 'Color',[1 0 0])
-  grid on; hold on; axis ([0, length(Slog_Re), -120, 0]); 
-  plot(Slog_Im, '-', 'LineWidth', 1, 'Color',[0 0 1])
-  grid on; hold on; axis ([0, length(Slog_Re), -120, 0]); 
-  title(['CORDIC SPECTRUM:'])
-  
-  subplot(4,1,3)
-  plot(Sidl_Re, '-', 'LineWidth', 1, 'Color',[1 0 0])
-  grid on; hold on; axis ([0, length(Sidl_Re), -120, 0]); 
-  plot(Sidl_Im, '-', 'LineWidth', 1, 'Color',[0 0 1])
-  grid on; hold on; axis ([0, length(Sidl_Re), -120, 0]); 
-  title(['IDEAL SPECTRUM:'])   
-  
-  subplot(4,1,4)
-  plot(ReSig-Id_cos, '-', 'LineWidth', 1, 'Color',[1 0 0])
-  grid on; hold on; axis tight; 
-  plot(ImSig-Id_sin, '-', 'LineWidth', 1, 'Color',[0 0 1])
-  grid on; hold on; axis tight; 
-  title(['DIFF - MODEL & THEORY:'])
+%  subplot(4,1,2)
+%  plot(Slog_Re, '-', 'LineWidth', 1, 'Color',[1 0 0])
+%  grid on; hold on; axis ([0, length(Slog_Re), -120, 0]); 
+%  plot(Slog_Im, '-', 'LineWidth', 1, 'Color',[0 0 1])
+%  grid on; hold on; axis ([0, length(Slog_Re), -120, 0]); 
+%  title(['CORDIC SPECTRUM:'])
+%  
+%  subplot(4,1,3)
+%  plot(Sidl_Re, '-', 'LineWidth', 1, 'Color',[1 0 0])
+%  grid on; hold on; axis ([0, length(Sidl_Re), -120, 0]); 
+%  plot(Sidl_Im, '-', 'LineWidth', 1, 'Color',[0 0 1])
+%  grid on; hold on; axis ([0, length(Sidl_Re), -120, 0]); 
+%  title(['IDEAL SPECTRUM:'])   
+%  
+%  subplot(4,1,4)
+%  plot(ReSig-Id_cos, '-', 'LineWidth', 1, 'Color',[1 0 0])
+%  grid on; hold on; axis tight; 
+%  plot(ImSig-Id_sin, '-', 'LineWidth', 1, 'Color',[0 0 1])
+%  grid on; hold on; axis tight; 
+%  title(['DIFF - MODEL & THEORY:'])
     
   
 %% -------------------------------- EOF ------------------------------------- %% 
--- math/cordic_test.m
+++ math/cordic_test.m
@@ -1,8 +1,8 @@
 clear all; close all;
 clc
 
-n = 15;				    % number of iterations
-N = 13;
+n = 12;				    % number of iterations
+N = 14;
 
 theta = 0;					% initial angle
 
@@ -16,25 +16,15 @@ theta = 0;					% initial angle
 
 % Run CORDIC algorith in rotation mode
 function [x_out, y_out, angle] = cordic(theta, n, N)
-	z = theta;
+	z = theta*8;
 	rot = 0;   % Indicates if a rotation occured. 0: no rotation 1: rotation by -pi 2: rotation by +pi
 
-	% Initial rotation
-%	if (z > pi/2) && (z <= pi)
-%		z = z - pi;
-%		rot = 1;
-%	end
-%	if (z < -pi/2) && (z >= -pi)
-%		z = z + pi;
-%		rot = 1;
-%	end
-
-	if (z > 2^(N-3)/2) && (z <= 2^(N-3))
-		z = z - 2^(N-3);
+	if (z > 2^(N-0)/2) && (z <= 2^(N-0))
+		z = z - 2^(N-0);
 		rot = 1;
 	end
-	if (z < -2^(N-3)/2) && (z >= -2^(N-3))
-		z = z + 2^(N-3);
+	if (z < -2^(N-0)/2) && (z >= -2^(N-0))
+		z = z + 2^(N-0);
 		rot = 1;
 	end
 
@@ -42,15 +32,19 @@ function [x_out, y_out, angle] = cordic(theta, n, N)
 	% Set x to 1 and y to 0
 	x = 2^(n-1);
 	y = 0;
-	for w = 0:n-1
-		if z < 0
-			x_next = x + round(y * 2^-w);
+  
+  
+	for w = 0:n
+    % z
+
+    if z < 0 
+      x_next = x + round(y * 2^-w);
       y_next = y - round(x * 2^-w);
-      z = z + 2^(N-3)*atan(2^-w)/pi;
+      z = z + round((2^(N)*atan(2^-w)/pi));
 		else
 		  x_next = x - round(y * 2^-w);
 		  y_next = y + round(x * 2^-w);
-      z = z - 2^(N-3)*atan(2^-w)/pi;
+      z = z - round((2^(N)*atan(2^-w)/pi));
 		end
 		x = x_next;
 		y = y_next;
@@ -64,31 +58,44 @@ function [x_out, y_out, angle] = cordic(theta, n, N)
 		y_out = -y;
 		
 	end
+  
+  x_out = round(x_out / 1.6467);
+  y_out = round(y_out / 1.6467);
+  
   angle = z;
 end
 
 
-%for w = 0:n-1
-%  Zz(w+1,1) = round((2^10)*atan(2^-w)/pi);	 
-%end
-%Angle_Int = int32(Zz);
-%Angle_Hex2 = dec2hex(Angle_Int);
+for w = 0:n
+  Zz(w+1,1) = (2^(N-3)*atan(2^-w)/pi);	 
+end
+
+for w = 0:n
+  Zx(w+1,1) = round((2^(N) * atan(2^-w)/pi));	 
+end
+Angle_Int = int32(Zx);
+Angle_Hex2 = dec2hex(Angle_Int);
 
 
 q = 1;
 %for theta = -pi : pi/2^(N-3) :pi-pi/2^(N-3)
-for theta = -2^(N-3) : 1 :2^(N-3)-1
+for theta = -2^(N-3) : 1 : 2^(N-3)-1
 	[x_out(q), y_out(q), angle(q)] = cordic(theta, n, N);
 	q = q + 1;
 end
 
-%th(:,1) = -2^(N-3) : 1 :2^(N-3)-1;
+th(:, 1) = (0 : 2^(N-2)-1);
+Id_sin(:,1) = round(2^(n-1) * sin(th(:,1) .* (2*pi) ./ (2^N/4) ) );
+Id_cos(:,1) = round(2^(n-1) * cos(th(:,1) .* (2*pi) ./ (2^N/4) ) );
+
+Id_cmps = abs(Id_sin + y_out');
+Id_cmpc = abs(Id_cos + x_out');
 
 
 %x_new = x_out + 1/2^(n) * randn(size(x_out));
 %y_new = y_out + 1/2^(n) * randn(size(y_out));
-X_new = round(x_out/1.6467);
-Y_new = round(y_out/1.6467);
+X_new = x_out;
+Y_new = y_out;
 max(Y_new)
 %Win = blackmanharris(length(x_out));
 %Win = kaiser(length(x_out), 9); 
@@ -105,27 +112,30 @@ Spec_Im = Spec_Im .* conj(Spec_Im);
 Spec_Re = fftshift(Spec_Re); 
 Spec_Im = fftshift(Spec_Im);
 
-%Sabs_Re = sqrt(real(Spec_Re).^2 .+ imag(Spec_Re).^2);
-%Sabs_Im = sqrt(real(Spec_Im).^2 .+ imag(Spec_Im).^2);
-Sabs_Re = Spec_Re;
-Sabs_Im = Spec_Im;
+Spec_Re = Spec_Re / max(Spec_Re);
+Spec_Im = Spec_Im / max(Spec_Im);
 
-Sabs_Re = Sabs_Re / max(Sabs_Re);
-Sabs_Im = Sabs_Im / max(Sabs_Im);
-
-Slog_Re = 10*log10(Sabs_Re);
-Slog_Im = 10*log10(Sabs_Im);
+Slog_Re = 10*log10(Spec_Re);
+Slog_Im = 10*log10(Spec_Im);
 
 figure(1) % Plot loaded data in Freq Domain
-  subplot(2,1,1)
+  subplot(3,1,1)
   plot(X_new, '-', 'LineWidth', 1, 'Color',[1 0 0])
   grid on; hold on; axis tight; 
   plot(Y_new, '-', 'LineWidth', 1, 'Color',[0 0 1])
   grid on; hold on; axis tight; 
   title(['CORDIC SINE / COSINE:'])
 
-  subplot(2,1,2)
+  subplot(3,1,2)
   plot(Slog_Re, '-', 'LineWidth', 1, 'Color',[1 0 0])
   grid on; hold on; axis ([0, length(Slog_Re), -160, 0]); 
   plot(Slog_Im, '-', 'LineWidth', 1, 'Color',[0 0 1])
   grid on; hold on; axis ([0, length(Slog_Re), -160, 0]);
+  title(['REAL / IMAG SPECTRUM:'])
+  
+  subplot(3,1,3)
+  plot(Id_cmps, '-', 'LineWidth', 1, 'Color',[1 0 0])
+  grid on; hold on; axis ([0, length(Id_cmps), -1, 7]); 
+  plot(Id_cmpc, '-', 'LineWidth', 1, 'Color',[0 0 1])
+  grid on; hold on; axis ([0, length(Id_cmps), -1, 7]);  
+  title(['ABS SIGNAL DIFFERENCE:'])
